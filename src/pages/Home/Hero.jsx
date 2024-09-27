4d5cbe1b3126aeb574b160e2bde875fe193558ef
uumarbek <koko87@mail.ru>
2024-09-27 20:25:40 +0500

    img

--- src/pages/Home/Hero.jsx
+++ src/pages/Home/Hero.jsx
@@ -11,7 +11,7 @@ const Hero = () => {
 
                 <div className='flex justify-center   mb-[5%] flex-col gap-10 items-center w-full mt-[11%] '>
 
-                    <h1 className='text-9xl'><img src="public/smile.png" alt="smile" /></h1>
+                    <h1 className='text-9xl'><img src="https://github.com/kinddevs2024/umarbek-portfolio/blob/main/public/smile.png?raw=true" alt="smile" /></h1>
                     <h1 className='text-4xl 	indent-3   text-center'>{t("I")}</h1>
                     <h1 className="sm:text-5xl font-extrabold text-xl md:max-w-[60%]  w-full text-center object-cover bg-[url('https://www.iconfinder.com/static/img/illustrations/marker2.svg?ba92f9ef8e')] bg- bg-no-repeat ">{t("gtext")}</h1>
                     <h1 className='sm:text-2xl  text-md font-extralight md:w-full  sm:max-w-[60%] w-full text-center'>{t("vtext")}</h1>
