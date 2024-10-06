61a456fd6a346c4eba5cad7dc351345e494a6e5c
umarbek <kinddevs2024@gmail.com>
2024-10-06 12:12:53 +0500

    Correct uz

--- src/utils/Il18n/uz/global.js
+++ src/utils/Il18n/uz/global.js
@@ -1,14 +1,14 @@
 const global = {
     projects: "loyihalar",
-    aboutme: "men haqimda",
-    I: "Hey men Umarbek 👏",
+    aboutme: "Men haqimda",
+    I: "Salom men Umarbek 👏",
     gtext : "Raqamli mahsulotlar, brendlar va tajriba yaratish.",
     vtext : "SFda mahsulot dizayneri va vizual dasturchi. Men UI/UX dizayni, sezgir veb-dizayn va vizual ishlab chiqishga ixtisoslashganman.",
-    myprojects : "mening loyihalarim",
-    getintouch : "bog'laning",
+    myprojects : "Mening loyihalarim",
+    getintouch : "Bog'laning",
     Aboutsme : "Men haqimda",
     order : "Buyutma berish",
-    contacts : "kontaktlar",
+    contacts : "Kontaktlar",
 
 }
 
